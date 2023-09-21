753dcca68cbd279306299be847dda83750d87986
rill <RillGd@gmail.com>
2023-09-21 22:54:34 +0200

    Initial commit

---
+++ src/main/java/kirill/zhdanov/websocketmessager/WebSocketMessagerApplication.java
@@ -0,0 +1,13 @@
+package kirill.zhdanov.websocketmessager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketMessagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketMessagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kirill/zhdanov/websocketmessager/WebSocketMessagerApplicationTests.java
@@ -0,0 +1,13 @@
+package kirill.zhdanov.websocketmessager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketMessagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
